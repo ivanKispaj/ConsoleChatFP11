772c016721b7dfca0272989be54d25781ed71fa3
Ivan Konishchev <kispaj@mail.ru>
2023-03-11 19:37:39 +0300

    Add change: User.cpp implemented methods: getUserId, getUserName, getUserLogin, getUserPassword, setUserName, setUserLogin.

--- Project/User.cpp
+++ Project/User.cpp
@@ -21,3 +21,45 @@ User::User(const std::string &name, const std::string &login, const std::string 
     setUserPassword(pass);
     setCurrentID();
 }
+
+/// @brief  public method
+/// @return return the user's ID
+int User::getUserId() const
+{
+    return _id;
+}
+
+/// @brief public method
+/// @return  return the user's name
+const std::string &User::getUserName() const
+{
+    return _name;
+}
+
+/// @brief public method
+/// @return // return the user's login
+const std::string &User::getUserLogin() const
+{
+    return _login;
+}
+
+/// @brief public method
+/// @return return the user's password in encoded form
+const std::string &User::getUserPassword() const
+{
+    return _pass;
+}
+
+/// @brief  set the user's name
+/// @param name string
+void User::setUserName(const std::string &name)
+{
+    _name = name;
+}
+
+/// @brief set the user's login
+/// @param login string
+void User::setUserLogin(const std::string &login)
+{
+    _login = login;
+}
