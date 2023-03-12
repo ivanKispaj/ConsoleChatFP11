7ce97702e777f2234fbed11a525550f3fcb7a1f0
Ivan Konishchev <kispaj@mail.ru>
2023-03-12 11:08:55 +0300

    Add change: User.cpp added documentation to methods.

--- Project/User.cpp
+++ Project/User.cpp
@@ -64,7 +64,7 @@ void User::setUserLogin(const std::string &login)
     _login = login;
 }
 
-/// @brief encoding and set the user's password
+/// @brief encoding and set the user's password this is private method, the DB class has access to
 /// @param pass unencoded password
 void User::setUserPassword(const std::string &pass)
 {
@@ -73,33 +73,47 @@ void User::setUserPassword(const std::string &pass)
     _pass = passWord;
 }
 
+/// @brief copies the already encoded password to the user property ( needed when the object is copied )
+///        private method, the DB class has access to
+/// @param pass
 void User::copyUserPassword(const std::string &pass)
 {
     _pass = pass;
 }
 
+/// @brief increases the message counter when adding messages to the database (DB)
+///        private method, the DB class has access to
 void User::addedMesage()
 {
     _countMessages++;
     _messageCount = _countMessages;
 }
 
+/// @brief To get the number of messages from the user - outgoing/incoming
+/// @return int outgoing/incoming count
 int User::getMessagesCount() const
 {
     return _messageCount;
 }
 
+/// @brief Creates a user ID when creating a user
+///        private method, the DB class has access to
 void User::setCurrentID()
 {
     _currentId++;
     _id = _currentId;
 }
 
+/// @brief sets the user ID when copying the user
+///        private method, the DB class has access to
+/// @param id int
 void User::setUserID(int id)
 {
     _id = id;
 }
 
+/// @brief sets the number of messages the user has when copying it
+/// @param cout int
 void User::setMessageCout(int cout)
 {
     _messageCount = cout;
