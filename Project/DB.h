d428f9921610ebe1a2e2b3cf2610e559cf745fce
ivanKispaj <kispaj@mail.ru>
2023-03-22 09:57:31 +0300

    Add change: class DB - fix addMessage, added - AddMessageToAllUsers. struct Message - added (id message, date message). struct User, Message, class DB - Documentation of methods moved from .cpp to .h

--- Project/DB.h
+++ Project/DB.h
@@ -12,30 +12,85 @@
 
 class DB
 {
-    DBCore<User> _userDB;
-    DBCore<Message> _messageDB;
+    DBCore<User> _userDB;       // database User
+    DBCore<Message> _messageDB; // database Message
 
 public:
+    /// @brief default init
     DB() = default;
+
+    /// @brief default deinit
     ~DB() = default;
+
+    /// @brief adds a new user to the database
+    /// @param name string
+    /// @param login string
+    /// @param pass string -> unencoded password
+    /// @return true - if is success, false if login non-unique!
     bool addUser(const std::string &name,
                  const std::string &login,
                  const std::string &pass);
+
+    /// @brief adds a new user to the database
+    /// @param user struct User
+    /// @return true - if is success, false if login non-unique!
     bool addUser(User &user);
-    void addMessage(const Message &message);
+
+    /// @brief adds a message to the database in a private chat
+    /// @param message struct Message type
+    void addMessage(Message &message);
+
+    /// @brief adds a message to the database in a public chat, for all users
+    /// @param message
+    void AddMessageToAllUsers(Message &message);
+
+    /// @brief Sets the user account password
+    /// @param userID int unique id
+    /// @param pass std::string new password
+    /// @return true - if is success, false - if there is no user with this id
     bool setUserPassword(int userID, const std::string &pass);
+
+    /// @brief Checks whether the login is unique.
+    /// @param login std::string.
+    /// @return true - if login unique, false - if the login exists.
     bool isUniqueLogin(const std::string &login);
+
+    /// @brief checks whether passwords match or not
+    /// @param userID int ID user
+    /// @param pass std::string incoming password
+    /// @return true - if the passwords match, false - if the passwords are different
     bool isCorrectPassword(int userID, const std::string &pass);
+
+    /// @brief Returns an array of users
+    /// @return  if the array is not empty - std::unique_ptr<User[]> , nullptr if the array is empty!
     const std::unique_ptr<User[]> getAllUsers() const;
+
+    /// @brief Get a user by login
+    /// @param login string -> user login
+    /// @param exception
+    /// @return Returning a pointer to a user object
     const std::unique_ptr<User> getUserByLogin(const std::string &login, bool exception = false) const;
+
+    /// @brief Returns the size of the user array
+    /// @return  int user array size
     int usersCount() const;
-    // Get all messages
+
+    /// @brief Returns an array of all messages
+    /// @return if the array is not empty - std::unique_ptr<Message[]>, nullptr if the array is empty!
     const std::unique_ptr<Message[]> getAllMessage() const;
-    // Get all user messages by login and user messages from senders in chronological ascending order
-    const std::unique_ptr<Message[]> getAllUserMessageByID(int id) const;
-    // Get all user messages by User ID
-    const std::unique_ptr<Message[]> getAllMessageByAuthorID(int authorID) const;
-    // Updates the user's data (name, login), except for the password!
+
+    /// @brief Returns an array of all incoming/outgoing messages for - user with ID, in chronological ascending order
+    /// @param id int unique recipientID
+    /// @return if the array is not empty - std::unique_ptr<Message[]>, nullptr if the array is empty!
+    const std::unique_ptr<Message[]> getAllMessageForUserById(int id) const;
+
+    /// @brief Updates the user's data (name, login), except for the password!
+    /// @param user struct User
+    /// @return true if successful, false if not a unique login, no user, no users
     bool updateUserData(const User &user);
+
+    /// @brief Deletes the user's account, marked as deleted. The password is reset, the name is changed to the "deleted"
+    /// @param user struct User
+    /// @return true if successful, false if not a unique login, no user, no users
     bool deleteUserAccount(User &user); // true -> if accaunt deleted, false -> failed
 };
