61f7ba6b6ca88b965edf22d799f0c6ed4c5f47b3
Fineralla <fineralla@inbox.ru>
2023-04-02 19:20:59 +0300

    Implemented a method for managing public messages

--- Project/ChatAdminInterface.cpp
+++ Project/ChatAdminInterface.cpp
@@ -20,6 +20,8 @@ chat::Results ChatAdminInterface::run(std::unique_ptr<DB> _db)
 
 void ChatAdminInterface::mainPage()
 {
+    system(clear);
+    pgDefault();
     UserInput<std::string, chat::Results> adminAreaPage(
         "Раздел администратора.",
         "Опции: "
@@ -37,6 +39,7 @@ void ChatAdminInterface::mainPage()
     auto result = chat::empty;
     do
     {
+        system(clear);
         result = adminAreaPage.IOgetline();
         switch (result)
         {
@@ -50,6 +53,8 @@ void ChatAdminInterface::mainPage()
             complaintManage();
             break;
         case chat::back:
+            system(clear);
+            pgDefault();
             return;
             break;
         default:
@@ -62,15 +67,7 @@ void ChatAdminInterface::complaintManage()
 {
     system(clear);
     pgDefault();
-    UserInput<std::string, chat::Results> complaintPage("Список жалоб",
-                                                        "Опции: "
-                                                        "\nу - удаление жалобы;"
-                                                        "\nнс - навигация по списку жалоб;"
-                                                        "\nп - управление пользователями;"
-                                                        "\nс - управление сообщениями;"
-                                                        "\nн - назад;"
-                                                        "\nУкажите опцию: ",
-                                                        "Неверный ввод", 5);
+    UserInput<std::string, chat::Results> complaintPage(std::string(), std::string(), "Неверный ввод", 5);
     complaintPage.addInputs("у", "нс", "п", "с", "н");
     complaintPage.addOutputs(chat::delete_message,
                              chat::chat_options,
@@ -79,19 +76,26 @@ void ChatAdminInterface::complaintManage()
                              chat::back);
     chat::Results result = chat::empty;
     std::string chatDescription;
+    std::string chatMainMessage;
     do
     {
         system(clear);
         auto complaintList = db->getAllPrivateMessagesForUserById(complaintBot->getId(), pg_MaxItems);
-
         messagesList(std::move(complaintList));
-
         if (pg_MaxItems <= 0)
         {
             std::cout << "Жалобы отсутствуют." << std::endl;
         }
         chatDescription = "Список жалоб. Показаны жалобы: " + pgInfo();
+        chatMainMessage = "Опции: " +
+                          std::string((pg_MaxItems <= 0) ? std::string() : "\nу - удалить жалобу;") +
+                          std::string((pg_MaxItems <= 0) ? std::string() : "\nнс - навигация по списку жалоб;") +
+                          "\nп - управление пользователями;"
+                          "\nс - управление сообщениями;"
+                          "\nн - назад;"
+                          "\nУкажите опцию: ";
         complaintPage.setDescription(chatDescription);
+        complaintPage.setMainMessage(chatMainMessage);
         result = complaintPage.IOgetline();
         switch (result)
         {
@@ -110,12 +114,12 @@ void ChatAdminInterface::complaintManage()
             usersManage();
             break;
         case chat::messages:
-            usersManage();
+            messagesManage();
             break;
         case chat::back:
+            system(clear);
+            pgDefault();
             return;
-            break;
-
         default:
             break;
         }
@@ -124,6 +128,55 @@ void ChatAdminInterface::complaintManage()
 
 void ChatAdminInterface::messagesManage()
 {
+    system(clear);
+    pgDefault();
+    UserInput<std::string, chat::Results> messagesPage(std::string(), std::string(), "Неверный ввод", 3);
+    messagesPage.addInputs("у", "нс", "н");
+    messagesPage.addOutputs(chat::delete_message,
+                            chat::chat_options,
+                            chat::back);
+    chat::Results result = chat::empty;
+    std::string chatDescription;
+    std::string chatMainMessage;
+    do
+    {
+        system(clear);
+        auto messages = db->getAllPublicMessages(pg_MaxItems);
+        messagesList(std::move(messages));
+        if (pg_MaxItems <= 0)
+        {
+            std::cout << "Публичные сообщения отсутствуют." << std::endl;
+        }
+        chatDescription = "Список сообщений. Показаны сообщения: " + pgInfo();
+        chatMainMessage = "Опции: " +
+                          std::string((pg_MaxItems <= 0) ? std::string() : "\nу - удалить сообщение;") +
+                          std::string((pg_MaxItems <= 0) ? std::string() : "\nнс - навигация по списку сообщений;") +
+                          "\nн - назад;"
+                          "\nУкажите опцию: ";
+        messagesPage.setDescription(chatDescription);
+        messagesPage.setMainMessage(chatMainMessage);
+        result = messagesPage.IOgetline();
+        switch (result)
+        {
+        case chat::delete_message:
+        {
+            if (pg_MaxItems > 0)
+            {
+                deleteMessage();
+            }
+        }
+        break;
+        case chat::chat_options:
+            pgNavigation();
+            break;
+        case chat::back:
+            system(clear);
+            pgDefault();
+            return;
+        default:
+            break;
+        }
+    } while (1);
 }
 
 void ChatAdminInterface::usersManage()
@@ -146,6 +199,11 @@ void ChatAdminInterface::deleteMessage(bool complaint_only)
             if (complaint_only && (message->getRecipientID() != 2))
             {
                 std::cout << "Сообщение не является жалобой." << std::endl;
+                continue;
+            }
+            if (!complaint_only && message->isPrivate)
+            {
+                std::cout << "Указан неверный messageId" << std::endl;
                 continue;
             }
             auto result = yesnoIO.IOgetline();
